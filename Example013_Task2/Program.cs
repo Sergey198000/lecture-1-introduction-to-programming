9704f7946bf0d3cc42e9a3e0ca539015b85e55c8
Sergey <vins2010@yandex.ru>
2022-08-23 18:50:09 +0500

    add Example 013 Task2

---
+++ Example013_Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()!);
+
+if (number1 > number2)
+{
+    Console.WriteLine($"Максимальное число = {number1}");
+}
+else if (number2 > number1)
+{
+    Console.WriteLine($"Максимальное число = {number2}");
+}
+else
+{
+    Console.WriteLine($"Введенные числа равны!");
+}

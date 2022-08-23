a65fa6c4078267b0d64d54a8356f7b98f6fde303
Sergey <vins2010@yandex.ru>
2022-08-24 00:02:30 +0500

    add Example 018 Task7

---
+++ Example018_Task7/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+
+if (n >= 100 || n < 1000)
+{
+    int a1 = n % 10;
+    Console.Write(a1);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}

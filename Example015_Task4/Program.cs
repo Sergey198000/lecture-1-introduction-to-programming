a2030604fca45532c9f5378ec304961d1cac499b
Sergey <vins2010@yandex.ru>
2022-08-23 20:19:23 +0500

    add Example 015 Task4

---
+++ Example015_Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+
+if (n % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}

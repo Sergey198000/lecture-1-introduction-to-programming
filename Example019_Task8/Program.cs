001c6434a7cce9900c8a6adc11aa81346b6834ce
Sergey <vins2010@yandex.ru>
2022-08-24 19:02:22 +0500

    add Example 019 Task8

---
+++ Example019_Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число обозначающее день недели от 1 до 7: ");
+int n = int.Parse(Console.ReadLine()!);
+
+if (n < 6 && n > 0)
+{
+    Console.WriteLine("Нет. Это будний день!");
+}
+else if (n > 5 && n <= 7)
+{
+    Console.WriteLine("Да. Это выходной день!");
+}    
+else
+{
+    Console.WriteLine("Введите число от 1 до 7. Такого дня недели нет!");
+}

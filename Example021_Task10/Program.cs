bfdcf8cd40187868809612b58bd9f806ea0d0526
Sergey <vins2010@yandex.ru>
2022-08-31 22:19:35 +0500

    add Example 021 Task10

---
+++ Example021_Task10/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int stepNumber = 1;
+
+if (number < 1)
+{
+    Console.WriteLine("Введите число больше 0");
+    return;
+}
+
+while (stepNumber <= number)
+{
+    Console.WriteLine($"Cube of (stepNumber) is: " + stepNumber * stepNumber * stepNumber);
+    stepNumber++;
+}

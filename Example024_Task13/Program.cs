a269ffeb3c6ed65dceeca85fbbff81cbd9fdced3
Sergey <vins2010@yandex.ru>
2022-09-15 18:40:45 +0500

    add Example024 Task13

---
+++ Example024_Task13/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i=3; i>=0; i--)
+{
+    sum+=number/(int)Math.Pow(10.0,i);
+    number=number%(int)Math.Pow(10.0,i);
+}
+Console.WriteLine(sum);
